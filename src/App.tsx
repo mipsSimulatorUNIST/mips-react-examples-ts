cb7d8d9ad31dc2095e11d142b02fa8dd73c8c045
Kang <rkdalsgur032@unist.ac.kr>
2023-02-26 12:35:33 +0900

    make Simulator Tab

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
+import { useState } from "react";
 import Assembler from "./components/Assembler/Assembler";
+import Simulator from "./components/simulator/Simulator";
 
 function App() {
+  const [on, setOn] = useState(true);
+  const handleOn = (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    setOn((prev) => !prev);
+  };
   return (
     <div>
-      <Assembler />
+      <button onClick={handleOn}>
+        convert to {on ? "Simulator" : "Assembler"}
+      </button>
+      {on ? <Assembler /> : <Simulator />}
     </div>
   );
 }
